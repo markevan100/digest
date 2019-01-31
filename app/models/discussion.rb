4f2cfc3fa1c0671dcc562ba5cb6d22aa220bb3bb
Mark Modak <mmodak@mmodak.local>
2019-01-30 21:28:54 -0700

    Added validations for user/discussion association.

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -1,4 +1,8 @@
 class Discussion < ApplicationRecord
   belongs_to :user
   has_many :responses
+
+  validates :title, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :question, presence: true, length: { minimum: 10, maximum: 300 }
+  validates :user_id, presence: true
 end
