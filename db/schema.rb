af1b1d502ffd1e6796a3cf7eeb5c8403b58be79a
Mark Modak <mmodak@mmodak.local>
2019-02-02 10:42:25 -0700

    Added secure password authentication to users.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_30_143041) do
+ActiveRecord::Schema.define(version: 2019_02_02_172204) do
 
   create_table "discussions", force: :cascade do |t|
     t.string "title"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 2019_01_30_143041) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
