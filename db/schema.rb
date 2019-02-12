a04560655b54e839c6b0e81a8e9955eb5e7d21b5
Mark Modak <mmodak@mmodak.local>
2019-02-11 19:27:57 -0700

    Added categories under TDD

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_11_132605) do
+ActiveRecord::Schema.define(version: 2019_02_12_004941) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "discussions", force: :cascade do |t|
     t.string "title"
